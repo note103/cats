59e77428752776f1638ea95a1fb85ba8e088a06e
KADOMATSU Hiroaki <note103@gmail.com>
2020-02-09 18:49:23 +0900

    Add a script

---
+++ cats.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use feature 'say';
+
+my $arg = $ARGV[0] // '';
+my @files;
+
+if ($arg =~ /-[al]/) {
+    @files = glob "* .*"; # 不可視ファイルも対象
+    if ($arg eq '-l') {
+        for (@files) {
+            say $_ if -f; # ファイル一覧だけ表示
+        }
+        exit;
+    }
+}
+else {
+    @files = glob "*"; # 不可視ファイル外す
+}
+
+my %cats;
+for (@files) {
+    $cats{$_} = `cat $_` if -f;
+}
+
+say for (sort keys %cats);
+say "===\n";
+
+for (sort keys %cats) {
+    say $_;
+    say '---';
+    say $cats{$_};
+}
